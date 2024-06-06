266715199625514081aab0e20bcc5d136107f1c0
tech <tech@d-2-tomern-1.rf.local>
2024-06-06 09:04:58 +0300

    Update missile icon

--- RafaelInvaders/rafael/iron_dome.gd
+++ RafaelInvaders/rafael/iron_dome.gd
@@ -19,7 +19,7 @@ func fire_lasers() -> void:
 	var enemies_in_range = area2d.get_overlapping_areas()
 	print("number of enemies: " + str(enemies_in_range.size()))
 	for enemy in enemies_in_range:
-			const missile = preload("res://rafael/iron_missile.tscn")
+			const missile = preload("res://rafael/arrow_missile.tscn")
 			var new_missile = missile.instantiate()
 			add_child(new_missile)
 			new_missile.update_target(enemy.global_position)	
